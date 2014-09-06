abff70ca24aef2003f2c7d100a74a2f4ce13efd8
jdgwartney <davidg@boundary.com>
2014-09-05 18:59:26 -0700

    Stub for JMX Agent

---
+++ src/main/java/com/boundary/jmx/agent/JMXAgent.java
@@ -0,0 +1,11 @@
+package com.boundary.jmx.agent;
+
+public class JMXAgent {
+
+  public JMXAgent() {
+  }
+
+  public static void main(String args[]) {
+      System.out.println("Running...");
+  }
+}
